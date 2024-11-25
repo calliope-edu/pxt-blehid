3c329e89c254fc4bc219203f5d95819a2fe4e1d3
Jørn Alraun <joern.alraun@calliope.cc>
2024-11-25 16:15:13 +0100

    Update mouse.ts
    translated

--- mouse.ts
+++ mouse.ts
@@ -2,8 +2,10 @@
 //% color=#8000FF 
 //% icon="\uf245"
 //% block="Mouse"
+//% block.loc.de="Maus"
 namespace mouse {
     //% blockId="mouse service" block="bluetooth mouse service"
+    //% block.loc.de="Bluetooth-Maus-Dienst"
     //% shim=mouse::startMouseService
     //% weight=50
     export function startMouseService() : void {
@@ -16,6 +18,7 @@ namespace mouse {
     }
 
     //% blockId="move mouse xy" block="move mouse | x by $x| and y by $y"
+    //% block.loc.de="Maus bewegen | x um $x| und y um $y"
     //% x.min=-127 x.max=127 y.min=-127 y.max=127 scroll.min=-127 scroll.max=127
     //% weight=40
     export function movexy(x: number, y: number) : void { 
@@ -23,24 +26,28 @@ namespace mouse {
     }
 
     //% blockId="mouse click" block="click mouse"
+    //% block.loc.de="click mouse"
     //% weight=30
     export function click() : void { 
         _send(0, 0, 0, 0x1);  
     }
 
     //% blockId="mouse middle click" block="middle click mouse"
+    //% block.loc.de="mittlerer Mausklick"
     //% weight=20
     export function middleClick() : void { 
         _send(0, 0, 0, 0x2);  
     }
     
     //% blockId="mouse right click" block="right click mouse"
+    //% block.loc.de="rechter Mausklick"
     //% weight=10
     export function rightClick() : void { 
         _send(0, 0, 0, 0x4);  
     }
 
     //% blockId="scroll mouse" block="scroll up |by $scroll"
+    //% block.loc.de="nach oben scrollen |by $scroll"
     //% scroll.min=-127 scroll.max=127
     //% weight=5
     export function scroll(scroll: number) : void { 
@@ -48,6 +55,7 @@ namespace mouse {
     }
 
     //% blockId="send mouse" block="send mouse motion|change x by $x|change y by $y|left pressed $left|middle pressed $middle|right pressed $right|change scroll by $scroll|hold buttons $hold" advanced=true
+    //% block.loc.de="Mausbewegung senden|X um $x ändern|Y um $y ändern|Links gedrückt $left|Mitte gedrückt $middle|Rechts gedrückt $right|Scroll um $scroll ändern|Tasten halten $hold"
     //% x.min=-127 x.max=127 y.min=-127 y.max=127 scroll.min=-127 scroll.max=127
     //% hold.default=false left.default=false middle.default=false right.default=false
     //% blockExternalInputs=true
@@ -58,6 +66,7 @@ namespace mouse {
     }
 
     //% blockID="mouse on status change" block="on mouse status change" advanced=true
+    //% block.loc.de="bei Änderung des Mausstatus"
     //% shim=mouse::setStatusChangeHandler 
     //% weight=20
     export function setStatusChangeHandler(a: Action) {
@@ -65,6 +74,7 @@ namespace mouse {
     }
 
     //% blockId="mouse enabled" block="mouse enabled" advanced=true
+    //% block.loc.de="Maus aktiviert"
     //% shim=mouse::isEnabled
     //% weight=30
      export function isEnabled() : boolean {
