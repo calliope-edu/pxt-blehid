62f40279c4684215609eefab97ad80245e1db82d
Jørn Alraun <joern.alraun@calliope.cc>
2024-11-25 16:29:08 +0100

    Update absmouse.ts
    translated

--- absmouse.ts
+++ absmouse.ts
@@ -2,8 +2,10 @@
 //% color=#ff0080 
 //% icon="\uf245"
 //% block="Absolute Mouse"
+//% block.loc.de="Absolute Maus"
 namespace absmouse {
     //% blockId="absolute mouse service" block="bluetooth absolute mouse service"
+    //% block.loc.de="Absoluter-Bluetooth-Maus-Dienst"
     //% shim=absmouse::startAbsoluteMouseService
     //% weight=50
     export function startAbsoluteMouseService() : void {
@@ -16,6 +18,7 @@ namespace absmouse {
     }
 
     //% blockId="move absolute mouse xy" block="move absolute mouse | x to $x| and y to $y"
+    //% block.loc.de="absolute Mausbewegung | x auf $x| und y auf $y"
     //% x.min=-32767 x.max=32767 y.min=-32767 y.max=32767
     //% weight=40
     export function movexy(x: number, y: number) : void { 
@@ -23,6 +26,7 @@ namespace absmouse {
     }
 
     //% blockId="absolute mouse click" block="click absolute mouse"
+    //% block.loc.de="absolute Mausklicks"
     //% weight=30
     export function click() : void { 
         // Sentinel values for x & y to use "last" x & y
@@ -30,6 +34,7 @@ namespace absmouse {
     }
 
     //% blockId="absolute mouse middle click" block="middle click absolute mouse"
+    //% block.loc.de="mittlerer Mausklick absolut"
     //% weight=20
     export function middleClick() : void { 
         // Sentinel values for x & y to use "last" x & y
@@ -37,6 +42,7 @@ namespace absmouse {
     }
     
     //% blockId="absolute mouse right click" block="right click absolute mouse"
+    //% block.loc.de="rechter Mausklick absolut"
     //% weight=10
     export function rightClick() : void { 
         // Sentinel values for x & y to use "last" x & y
@@ -44,6 +50,7 @@ namespace absmouse {
     }
 
     //% blockId="send absolute mouse" block="send absolute mouse motion|set x to $x| and set y to $y|left pressed $left|middle pressed $middle|right pressed $right|hold buttons $hold" advanced=true
+    //% block.loc.de="sende absolute Mausbewegung|setze x auf $x| und setze y auf $y|links gedrückt $left|mittler gedrückt $middle|rechts gedrückt $right|halte Tasten $hold"
     //% x.min=-32767 x.max=32767 y.min=-32767 y.max=32767
     //% hold.default=false left.default=false middle.default=false right.default=false
     //% blockExternalInputs=true
@@ -54,6 +61,7 @@ namespace absmouse {
     }
 
     //% blockID="absolute mouse on status change" block="on absolute mouse status change" advanced=true
+    //% block.loc.de="bei absoluter Statusänderung der Maus"
     //% shim=absmouse::setStatusChangeHandler 
     //% weight=20
     export function setStatusChangeHandler(a: Action) {
@@ -61,6 +69,7 @@ namespace absmouse {
     }
 
     //% blockId="absolute mouse enabled" block="absolute mouse enabled" advanced=true
+    //% block.loc.de="absolute Maus aktiviert"
     //% shim=absmouse::isEnabled
     //% weight=30
      export function isEnabled() : boolean {
